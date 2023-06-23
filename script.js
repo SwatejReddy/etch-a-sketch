efb497ee8355fa38a527adeccc14c343daa1d3f3
SwatejReddy <109172700+SwatejReddy@users.noreply.github.com>
2023-06-24 00:21:23 +0530

    Update script.js
    added magicButton functionality

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const gridContainer = document.querySelector(".grid-container");
 const clearBtn = document.querySelector(".clear-btn");
+const eraserBtn = document.querySelector(".eraser-btn");
+const colorModeBtn = document.querySelector(".color-mode-btn");
+const colorPicker = document.querySelector(".color-picker");
+const magicModeBtn = document.querySelector(".magic-mode-btn");
+
+var currColor = colorPicker.value;
 var gridCount = 1;
 var isMouseDown = false;
+var flag = 1;
+
+let colorInterval;
 
 
 //creates a grid:
@@ -14,13 +23,12 @@ for (let i = 0; i < 64; i++) {
 }
 
 const boxes = document.querySelectorAll(".box");
-const eraserBtn = document.querySelector(".eraser-btn");
-const colorModeBtn = document.querySelector(".color-mode-btn");
-const colorPicker = document.querySelector(".color-picker");
-var currColor = colorPicker.value;
+
 
 //everytime color picker is used, it will update the currColor:
 colorPicker.addEventListener('change', function(event) {
+  //stop magic mode and then update the new color
+  stopMagicColor();
   const selectedColor = event.target.value;
   currColor = selectedColor;
 });
@@ -50,9 +58,6 @@ function colorIt(){
   });
 }
 
-//We are calling the 'colorIt' when the page loads with the default color in 'currColor':
-colorIt();
-
 //Wehen ever this is invoked, and then as we press / and hold the grid will be colored with white (erased)
 function deColorIt(){
   document.addEventListener('mousedown', function(event) {
@@ -79,21 +84,58 @@ function deColorIt(){
   
 }
 
+//We are calling the 'colorIt' when the page loads with the default color in 'currColor':
+colorIt();
 
-//whenever clearBtn is pressed, the entire grid will be set to white:
-clearBtn.addEventListener("click", function(){
-  boxes.forEach((box) => {
-    box.style.backgroundColor = "white";
-  });  
+//whenever again, colorModeBtn is pressed, 'colorIt()' is invoked, enabling the coloring with whatever color is present in 'currColor':
+colorModeBtn.addEventListener("click", function(){
+  //stop magic mode and then update currColor to whatever color the picker has:
+  stopMagicColor();
+  currColor = colorPicker.value;
+
+  colorIt();
 })
 
 //whenever eraserBtn is pressed, 'deColorIt()' is called, enabling the de-coloring to white:
 eraserBtn.addEventListener("click", function(){
+  //stop magic mode and then update currColor to whatever color the picker has:
+  stopMagicColor();
+  currColor = colorPicker.value;
+
   deColorIt();
 })
 
+//generates a random color:
+function randColor(){
+  const red = Math.floor(Math.random()*256)
+  const green = Math.floor(Math.random()*256)
+  const blue = Math.floor(Math.random()*256)
 
-//whenever again, colorModeBtn is pressed, 'colorIt()' is invoked, enabling the coloring with whatever color is present in 'currColor':
-colorModeBtn.addEventListener("click", function(){
-  colorIt();
+  const color = `rgb(${red},${green},${blue})`;
+  return color;
+}
+
+//whenever clearBtn is pressed, the entire grid will be set to white:
+clearBtn.addEventListener("click", function(){
+  //stop magic mode and then update currColor to whatever color the picker has:
+  stopMagicColor();
+  currColor = colorPicker.value;
+
+  boxes.forEach((box) => {
+    box.style.backgroundColor = "white";
+  });
+})
+
+
+//when invoked, stops magic color:
+function stopMagicColor(){
+  clearInterval(colorInterval);
+}
+
+//upon clicking 'magicModeBtn', starts calling 'randColor' every 50ms:
+magicModeBtn.addEventListener("click", function(){
+    colorIt();
+    colorInterval = setInterval(() => {
+    currColor = randColor();
+    }, 10);
 })
